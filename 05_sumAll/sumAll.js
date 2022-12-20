69046f40c01c72dfcdae60c0e4eef501d32b123d
volpe23 <arozmianecs@gmail.com>
2022-12-20 19:19:48 +0200

    Ex 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let res = 0;
+    if (first > last) {
+        const placeholder = first
+        first = last
+        last = placeholder
+    }
+    else if (typeof(first) !== 'number' || typeof(last) !== 'number' || first < 0 || last < 0) return "ERROR"
+    for (let i = first; i <= last; i++) {
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
