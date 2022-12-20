4637213be7936d0f0ed22fbbc4e3bfac20021403
volpe23 <arozmianecs@gmail.com>
2022-12-20 17:57:17 +0200

    Third and fourth exrcise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let res = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        res = res + string[i]
+    }
+    return res
 };
-
+// let inp = require('prompt-sync') ();
+// const string = inp("Input here ")
+// console.log(reverseString(string))
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, inp1, inp2, inp3, inp4) {
+    const len = arguments.length
+    let res = arr;
+    for (let i = 1; i < len; i++) {
+        arguments[i] = arguments[i] || undefined
+        console.log(arguments[i], typeof(arguments[i]))
+        if (arr.includes(arguments[i])) {
+            console.log("Match")
+            const index = arr.indexOf(arguments[i])
+            res.splice(index, 1)
+        }
+    }
+    return res
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
