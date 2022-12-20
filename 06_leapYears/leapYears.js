33373abc7e4623cf8e6f64cef7387a057a04bb16
volpe23 <arozmianecs@gmail.com>
2022-12-20 19:28:07 +0200

    Ex 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const date = new Date(year, 2, 0)
+    const days = date.getDate()
+    if (days === 28) return false
+    else if (days === 29) return true
+    
 };
 
 // Do not edit below this line
