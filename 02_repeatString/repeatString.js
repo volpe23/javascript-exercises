5edb23b84d2c617bfaa6eec5e7120a12d096d2ec
volpe23 <arozmianecs@gmail.com>
2022-12-20 16:58:48 +0200

    Exercise 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, nums) {
+    if (nums < 0) return "ERROR"
+    let res = '';
+    for (let i = 0; i < nums; i++) {
+        res = res + string
+        // console.log(string)
+    }
+    return res
 };
 
 // Do not edit below this line
