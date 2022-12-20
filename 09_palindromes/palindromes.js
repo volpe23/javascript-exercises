ba83984421fa2762f00a567d32370b44d1c15642
volpe23 <arozmianecs@gmail.com>
2022-12-20 21:27:48 +0200

    Ex 9 and 10 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
+    let rawString = word.replace(regex, '')
+    const len = rawString.length
+    let isPalindrome = true;
+    for (let i = 0; i < len / 2; i++) {
+        if (rawString[i].toLowerCase() === rawString[len - 1 - i].toLowerCase()) {
+            
+            continue;
+        }
+        else {
+            isPalindrome = false;
+            break;
+        }
+        
+    }
+    return isPalindrome
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(ind) {
+    if (ind < 0) return 'OOPS'
+    let fibs = [1, 1];
+    let len = fibs.length;
+    while (len <= ind - 1) {
+        newFib = fibs[fibs.length - 1] + fibs[fibs.length - 2];
+        fibs.push(newFib)
+        len++;
+    }
+    return fibs[ind - 1]
 };
-
-// Do not edit below this line
 module.exports = fibonacci;
