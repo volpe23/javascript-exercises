cf812f5c98f19985eee26c3a19dc0031b29e7095
volpe23 <arozmianecs@gmail.com>
2022-12-26 21:18:59 +0200

    Ex 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((oldest, curr) => {
+        const currDeath = curr.yearOfDeath || (new Date().getFullYear());
+        const oldDeath = oldest.yearOfDeath || (new Date().getFullYear());
+        
+        const oldestAge = oldDeath - oldest.yearOfBirth;
+        const currAge = currDeath - curr.yearOfBirth;
 
+        return currAge > oldestAge ? curr : oldest;
+    })
+    return oldest
 };
 
 // Do not edit below this line
